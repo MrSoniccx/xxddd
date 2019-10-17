d99418273d0863726a7343fed61749197d2bfe68
20161012 LEONARDO YAHEL ISORDIA AVILA <20161012@TELEMATICA.UCOL.MX>
2019-10-17 18:28:53 -0500

    otro botonsito

--- ciclo-for.js
+++ ciclo-for.js
@@ -1,10 +1,11 @@
 var btnUno = document.querySelector('#btnUno')
 var btnDos = document.querySelector('#btnDos')
+var btnTres = document.querySelector('#btnTres')
 
 
 btnUno.addEventListener('click', () =>
 {
-    let num1 = document.querySelector('#num1').value
+    let num1 = 1
 
     let resultado = document.querySelector('#resultado')
     resultado.value = null
@@ -20,4 +21,17 @@ btnDos.addEventListener('click', () =>{
 for(let i=0; i <= 100; i=i+5){
     resultado.value = (resultado.value + i)+", ";
 }
+})
+
+btnTres.addEventListener('click', () =>
+{
+    let num1 = document.querySelector('#num1').value
+    let num2 = document.querySelector('#num2').value
+
+    let resultado = document.querySelector('#resultado')
+    resultado.value = null
+    for(num1 ; num1 <= num2; num1++)
+    {
+    resultado.value = resultado.value+" " + num1
+    }
 })
